18eec6d08b790dcff4d2e8b7ac6b2f1973522125
Stephen Chou <3065482@qq.com>
2018-06-30 06:39:50 +0800

    initial

--- Python_Practise/pa_pexels_mysql.py
+++ Python_Practise/pa_pexels_mysql.py
@@ -5,8 +5,11 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import re
 from concurrent.futures import ProcessPoolExecutor
 import pymysql
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
-urls = ['https://www.pexels.com/collections/botany-9hb3lwm/?page={}'.format(n) for n in range(1,20)]
+urls = ['https://www.pexels.com/popular-photos/?page={}'.format(n) for n in range(1,100)]
 header = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36"}
 
 
@@ -18,8 +21,8 @@ def get_info(url):
             img = item.attrs['data-large-src']
             pic_urls = re.findall(r'https://.*\.(?:jpeg|jpg)',img)
             for pic_url in pic_urls:
-                print("inserting %s into mysql DB" % title)
-                conn = pymysql.connect(host='47.94.80.95', port=3333, user='root', passwd='Xiaoxian0910', db='pexels', charset='utf8')
+                logging.info("inserting %s into mysql DB" % title)
+                conn = pymysql.connect(host='47.94.80.95', port=3333, user='root', passwd='', db='pexels', charset='utf8')
                 cursor = conn.cursor()
                 sql = "INSERT INTO pexels.popular_pics (title, pic_url) VALUES(%s,%s)"
                 cursor.execute(sql, (title,pic_url))
@@ -28,5 +31,5 @@ def get_info(url):
                 conn.close()
 
 if __name__ == "__main__":
-    pool = ProcessPoolExecutor(max_workers=10)
+    pool = ProcessPoolExecutor(max_workers=20)
     pool.map(get_info,urls)
