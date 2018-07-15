28c66d847fbc9e04ee50637b10458469c4d044c6
Stephen Chou <3065482@qq.com>
2018-07-15 17:25:56 +0800

    initial

--- Python_Practise/caipiao2.py
+++ Python_Practise/caipiao2.py
@@ -2,8 +2,10 @@ import random
 import xlrd
 # _*_ coding:utf-8 _*_
 test2 = []
-qiu = []
+qiu_result_list = []
+
 k = 0
+
 
 def getExcel():
     ExcelFile = xlrd.open_workbook('/Users/StephenChou/PycharmProjects/Python_Study/Python_Practise/test.xls')
@@ -15,41 +17,42 @@ def getExcel():
     for i in range(sheet.nrows):
         rows = sheet.row_values(i)
         test2.append(rows)
-    print(len(test2))
+    # print(len(test2))
     return test2
 
 def GenBall():
+    qiu_result_list = []
+    qiu = []
     org_str = '7 8 20 26 30 29 9 14 19 17 25 30 4 10 16 23 28 31 6 13 14 20 27 32 5 12 18 22 26 33'
     nums = ['13', '12', '14', '15', '16']
     org_list = org_str.split(" ")
     org_list_after = set(list(org_list))
-    while True:
+    for m in range(1,7):
         hong = random.choice(list(org_list_after))
         qiu.append(hong)
-        if len(qiu) == 6:
-            set(list(qiu))
-            break
+        qiu = list(set(qiu))
     lan = random.choice(nums)
     qiu.append(lan)
-    # print(qiu)
-    return qiu
+    qiu_result = ''.join(qiu)
+    qiu_result_list.append(qiu_result)
+    print(qiu_result_list)
+    return qiu_result_list
 
 
-
-def compareBall():
-    global k
-    if ''.join(qiu) in test2:
-        print("got it")
-    else:
+if __name__ == "__main__":
+    getExcel()
+    while True:
+        GenBall()
         k += 1
-        print(k)
-        print("current qiu is {}".format(''.join(qiu)))
-        print(test2)
+        if qiu_result_list in test2:
+            print(k)
+            print("got it")
+            break
+        else:
+            print(qiu_result_list)
+            print(k)
 
 
-getExcel()
-while True:
-    GenBall()
-    compareBall()
+
 
 
