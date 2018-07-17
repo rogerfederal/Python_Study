f0c2a8d6ff96ee63a63baa0e5f47b7259597f547
zking2000 <3065482@qq.com>
2018-07-17 22:50:20 +0800

    initial

--- Python_Practise/wechat_xiangkang.py
+++ Python_Practise/wechat_xiangkang.py
@@ -1,6 +1,6 @@
 from wxpy import *
 
-bot = Bot(cache_path=False)
+bot = Bot(console_qr=True,cache_path=True)
 
 @bot.register(chats=None,msg_types=TEXT,except_self=True,enabled=True)
 def print_chat_msg(msg):
