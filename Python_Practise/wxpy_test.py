9e1c6c30489c59501adea30a4e945c9578bc9f2e
zking2000 <3065482@qq.com>
2018-07-17 08:10:51 +0800

    initial

--- Python_Practise/wxpy_test.py
+++ Python_Practise/wxpy_test.py
@@ -7,10 +7,13 @@ my_friend = bot.friends().search(u'Vishal S')[0]
 # my_friend.send(u"测试消息")
 
 # print(bot.mps()) #列出所有公众号
-bot.groups(update=True, contact_only=False)
-@bot.register()
-def print_others(msg):
-  print(msg)
+
+#####reply wechat message#######
+@bot.register(bot.self,except_self=False)
+def reply_self(msg):
+  return 'i see u'
+embed()
+#######reply wechat message#####
 
 
 # Message = bot.messages.search(sender=bot.self)
