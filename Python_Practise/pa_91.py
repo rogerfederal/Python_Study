82c33c680f7c438da4f66d86ac4b14824c88ebab
Stephen Chou <3065482@qq.com>
2018-07-22 21:53:14 +0800

    initial

--- Python_Practise/pa_91.py
+++ Python_Practise/pa_91.py
@@ -7,12 +7,12 @@ from time import sleep
 import datetime
 import pymysql
 from concurrent.futures import ProcessPoolExecutor
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+# import logging
+# logging.basicConfig(level=logging.DEBUG,
+#                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
-urls = ['http://91porn.com/v.php?next=watch&page={}'.format(n) for n in range(1,4333)]
-header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36","Cookie":"__cfduid=dbf2d1a6481e61c9feea0ce09c65ed1d71531586998; CLIPSHARE=0596ro750i6p32jbejsn95dkg1; __utmc=50351329; __utmz=50351329.1531587012.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __dtsu=D9E9B66B1884C05A1A43BE9002095F67; language=cn_CN; evercookie_cache=<br />; evercookie_png=<br />; evercookie_etag=<br />; DUID=ea293KLP7pOBstMDdswA2GrK80urlWzK%2F2PHfHOBZ5pjZYmQ; USERNAME=a104KJr4EiqAVXpAQG6PLccdYp%2FFHT0jOZhiwuJymXx0vIzd7Ps; EMAILVERIFIED=no; l91lb91a=1; __utma=50351329.784225177.1531587012.1531587012.1531609867.2; __utmb=50351329.0.10.1531609867"}
+urls = ['http://91porn.com/v.php?next=watch&page={}'.format(n) for n in range(5,4333)]
+header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"}
 
 def getInfo(url):
     response = requests.get(url,headers=header)
