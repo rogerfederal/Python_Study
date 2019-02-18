f2973d8869e0df15f4020f428e7081068698a1ea
zking2000 <3065482@qq.com>
2019-02-18 21:47:12 +0800

    update

--- Python_Practise/base64_method.py
+++ Python_Practise/base64_method.py
@@ -1,6 +1,7 @@
 import base64
 
-result = base64.b64encode(b'username:password')
-result2 = base64.b64decode('passcode')
-print(result)
-print(result2)
+# result = base64.b64encode(b'username:password')
+result2 = base64.b64decode('')
+# print(result)
+# print(result2)
+print(result2.decode('utf-8','ignore'))
