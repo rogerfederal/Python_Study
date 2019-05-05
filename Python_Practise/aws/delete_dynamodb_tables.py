dab4811683d36eac4006d80a40ee10dda2ee1f2e
zking2000 <3065482@qq.com>
2019-05-05 11:25:06 +0800

    update

--- Python_Practise/aws/delete_dynamodb_tables.py
+++ Python_Practise/aws/delete_dynamodb_tables.py
@@ -1,12 +1,20 @@
 import boto3
 import os
 import json
+import pysnooper
 
 dynamodb = boto3.resource('dynamodb')
 
+@pysnooper.snoop()
+def check():
+    tables = os.popen(r'aws dynamodb list-tables').read()
+    results = json.loads(tables)
+    return results['TableNames']
+    # for table in results['TableNames']:
+    #     j = os.popen(r'aws dynamodb delete-table --table-name {}'.format(table)).read()
+    #     print(j)
 
-tables = os.popen(r'aws dynamodb list-tables').read()
-results = json.loads(tables)
-for table in results['TableNames']:
-    j = os.popen(r'aws dynamodb delete-table --table-name {}'.format(table)).read()
-    print(j)
+
+
+if __name__ == "__main__":
+    check()
